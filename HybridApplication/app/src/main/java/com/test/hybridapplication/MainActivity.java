014395c726369ef49758f9568e61cff6ff212918
hidongyoon31 <hidongyoon31@naver.com>
2019-06-10 23:49:18 +0900

    Add testno11

--- HybridApplication/app/src/main/java/com/test/hybridapplication/MainActivity.java
+++ HybridApplication/app/src/main/java/com/test/hybridapplication/MainActivity.java
@@ -21,6 +21,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final int BT_FRAGMENT5 = 9;
     private final int BT_FRAGMENT6 = 10;
     private final int BT_FRAGMENT7 = 11;
+    private final int BT_FRAGMENT8 = 12;
+
+
 
 
     private Button top_tab1, top_tab2, top_tab3, top_tab4;
